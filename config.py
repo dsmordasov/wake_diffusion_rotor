773392e3cd0226a1f8f31f51991789e90ae0ead2
Dmitrij <d.s.mordasov@gmail.com>
2022-03-14 20:38:43 +0100

    add option to use config.py locally
    idea for tomorrow - check OS, if Windows, run locally (do not import PWES), if not, import it, this way it always runs on gbar without chaning the code!

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 This file was made for increased ease of automating PyWakeEllipSys simulations.
 """
 import numpy as np
-import matlotlib as mpl
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 #%% Plot style, generously donated by Mads Christian Baungaard
@@ -16,103 +16,128 @@ plt.rcParams['axes.grid']=True
 yd = dict(rotation=0,ha='right') # I couldn't find a way to customize these, so use a dict everytime..
 plt.close('all')
 
-
-#%% Wind turbine definition
-from py_wake_ellipsys.wind_farm_models.ellipsys_lib.ellipsys_wind_turbines import EllipSysOneTypeWT
-from py_wake_ellipsys.examples.data.turbines.ADairfoil import ADairfoil_path
-
-
-# DTU10MW for Joukowski `2xxx` AD forcing
-from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_ct_curve
-from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_power_curve
-from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_rpm_curve
-from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_pitch_curve
-from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_bladeloading
-
-class DTU10MW(EllipSysOneTypeWT):
-    def __init__(self):
-        EllipSysOneTypeWT.__init__(self,
-        name='DTU10MW',
-        diameter=178.3,
-        hub_height=119.0,
-        cutin=4.0,
-        cutout=25.0,
-        dws=1.0, # what is this?
-        tilt=0.0,
-        rated_power=10000.0, # 10e6 [W], but this is in [kW] due to our choice of `power_unit`
-        rotdir='cw',
-        power_unit='kW',
-        ct_func=self.dtu10mw_ct,
-        power_func=self.dtu10mw_power,
-        rpm_func=self.dtu10mw_rpm_curve,
-        pitch_func=self.dtu10mw_pitch,
-        bladeloading_func=self.dtu10mw_bladeloading,
-        airfoildata_file='dtu10mw.pc',
-        airfoildata_path=ADairfoil_path,
-        airfoildata_file_type=2,  # 1=flex5, 2=hawc2
-        bladegeo_file='dtu10mw.geo',
-        bladegeo_path=ADairfoil_path
-        ) # Below are re-definitions for ease of access
-        self.D=178.3
-        self.zRef=119.0
+#%% To enable post-processing locally, not on gbar, we can not try to import PWES files
+local = True # Change to False when running on gbar
+if not local:
+    # Wind turbine imports
+    from py_wake_ellipsys.wind_farm_models.ellipsys_lib.ellipsys_wind_turbines import EllipSysOneTypeWT
+    from py_wake_ellipsys.examples.data.turbines.ADairfoil import ADairfoil_path
     
-    def dtu10mw_ct(self, u):
-        return np.interp(u, dtu10mw_ct_curve[:, 0], dtu10mw_ct_curve[:, 1])
-
-    def dtu10mw_power(self, u):
-        return np.interp(u, dtu10mw_power_curve[:, 0], dtu10mw_power_curve[:, 1])
-
-    def dtu10mw_rpm_curve(self, u):
-        return np.interp(u, dtu10mw_rpm_curve[:, 0], dtu10mw_rpm_curve[:, 1])
+    # DTU10MW 
+    from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_ct_curve
+    from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_power_curve
+    from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_rpm_curve
+    from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_pitch_curve
+    from py_wake_ellipsys.examples.data.turbines.dtu10mw import dtu10mw_bladeloading
     
-    def dtu10mw_pitch(self, u):
-        return np.interp(u, dtu10mw_pitch_curve[:, 0], dtu10mw_pitch_curve[:, 1])
+    # NREL5MW
+    from py_wake_ellipsys.examples.data.turbines.nrel5mw import nrel5mw_ct_curve
+    from py_wake_ellipsys.examples.data.turbines.nrel5mw import nrel5mw_power_curve
+    from py_wake_ellipsys.examples.data.turbines.nrel5mw import nrel5mw_rpm_curve
     
-    def dtu10mw_bladeloading(self, r):
-        Qx = np.interp(r, dtu10mw_bladeloading[:, 0], dtu10mw_bladeloading[:, 1])
-        Qy = np.interp(r, dtu10mw_bladeloading[:, 0], dtu10mw_bladeloading[:, 2])
-        Qz = np.interp(r, dtu10mw_bladeloading[:, 0], dtu10mw_bladeloading[:, 3])
-        return Qx, Qy, Qz
+    # Sites
+    from py_wake.examples.data.hornsrev1 import Hornsrev1Site
+    
+    #%% Wind turbine definitions
+    class DTU10MW(EllipSysOneTypeWT):
+        def __init__(self):
+            EllipSysOneTypeWT.__init__(self,
+            name='DTU10MW',
+            diameter=178.3,
+            hub_height=119.0,
+            cutin=4.0,
+            cutout=25.0,
+            dws=1.0, # what is this?
+            tilt=0.0,
+            rated_power=10000.0, # 10e6 [W], but this is in [kW] due to our choice of `power_unit`
+            rotdir='cw',
+            power_unit='kW',
+            ct_func=self.dtu10mw_ct,
+            power_func=self.dtu10mw_power,
+            rpm_func=self.dtu10mw_rpm_curve,
+            pitch_func=self.dtu10mw_pitch,
+            bladeloading_func=self.dtu10mw_bladeloading,
+            airfoildata_file='dtu10mw.pc',
+            airfoildata_path=ADairfoil_path,
+            airfoildata_file_type=2,  # 1=flex5, 2=hawc2
+            bladegeo_file='dtu10mw.geo',
+            bladegeo_path=ADairfoil_path
+            ) # Below are re-definitions for ease of access
+            self.D=178.3
+            self.zRef=119.0
+        
+        def dtu10mw_ct(self, u):
+            return np.interp(u, dtu10mw_ct_curve[:, 0], dtu10mw_ct_curve[:, 1])
+    
+        def dtu10mw_power(self, u):
+            return np.interp(u, dtu10mw_power_curve[:, 0], dtu10mw_power_curve[:, 1])
+    
+        def dtu10mw_rpm_curve(self, u):
+            return np.interp(u, dtu10mw_rpm_curve[:, 0], dtu10mw_rpm_curve[:, 1])
+        
+        def dtu10mw_pitch(self, u):
+            return np.interp(u, dtu10mw_pitch_curve[:, 0], dtu10mw_pitch_curve[:, 1])
+        
+        def dtu10mw_bladeloading(self, r):
+            Qx = np.interp(r, dtu10mw_bladeloading[:, 0], dtu10mw_bladeloading[:, 1])
+            Qy = np.interp(r, dtu10mw_bladeloading[:, 0], dtu10mw_bladeloading[:, 2])
+            Qz = np.interp(r, dtu10mw_bladeloading[:, 0], dtu10mw_bladeloading[:, 3])
+            return Qx, Qy, Qz
+    
+    
+    class NREL5MW(EllipSysOneTypeWT):
+        def __init__(self):
+            EllipSysOneTypeWT.__init__(self, name='NREL5MW',
+            diameter=126.0,
+            hub_height=90.0,
+            cutin=4.0,
+            cutout=25.0,
+            dws=1.0,
+            tilt=0.0,
+            rated_power=5000.0,
+            rotdir='cw',
+            power_unit='kW',
+            ct_func=self.nrel5mw_ct,
+            power_func=self.nrel5mw_power,
+            rpm_func=self.nrel5mw_rpm,
+            # input on pitch function etc is not needed for non-3xxx forcings, but they are required positional arguments
+            pitch_func='',
+            bladeloading_func='',
+            airfoildata_file='',
+            airfoildata_path='',
+            airfoildata_file_type='',  # 1=flex5, 2=hawc2
+            bladegeo_file='',
+            bladegeo_path=''
+            ), # Below are re-definitions for ease of access
+            self.D=126.0
+            self.zRef=90.0
+    
+        def nrel5mw_ct(self, u):
+            return np.interp(u, nrel5mw_ct_curve[:, 0], nrel5mw_ct_curve[:, 1])
+    
+        def nrel5mw_power(self, u):
+            return np.interp(u, nrel5mw_power_curve[:, 0], nrel5mw_power_curve[:, 1])
+    
+        def nrel5mw_rpm(self, u):
+            return np.interp(u, nrel5mw_rpm_curve[:, 0], nrel5mw_rpm_curve[:, 1])
+        
+    # Wind farm definions
+    class Single_Hornsrev1_wt(Hornsrev1Site):
+        def __init__(self):
+            Hornsrev1Site.__init__(self)
+            self.Ti=0.06
+            self.wt_x=[0]
+            self.wt_y=[0]
+            self.type_i=np.array([0])
+    
+    class TwobyTwo_Hornsrev1_wf(Hornsrev1Site):
+        def __init__(self, wt):
+            Hornsrev1Site.__init__(self)
+            self.Ti=0.06
+            self.wt_x=[0, 5*wt.D, 0, 5*wt.D]
+            self.wt_y=[0, 0, 5*wt.D, 5*wt.D]
+            self.type_i=np.array([0, 0, 0, 0])
 
-from py_wake_ellipsys.examples.data.turbines.nrel5mw import nrel5mw_ct_curve
-from py_wake_ellipsys.examples.data.turbines.nrel5mw import nrel5mw_power_curve
-from py_wake_ellipsys.examples.data.turbines.nrel5mw import nrel5mw_rpm_curve
-
-class NREL5MW(EllipSysOneTypeWT):
-    def __init__(self):
-        EllipSysOneTypeWT.__init__(self, name='NREL5MW',
-        diameter=126.0,
-        hub_height=90.0,
-        cutin=4.0,
-        cutout=25.0,
-        dws=1.0,
-        tilt=0.0,
-        rated_power=5000.0,
-        rotdir='cw',
-        power_unit='kW',
-        ct_func=self.nrel5mw_ct,
-        power_func=self.nrel5mw_power,
-        rpm_func=self.nrel5mw_rpm,
-        # input on pitch function etc is not needed for non-3xxx forcings, but they are required positional arguments
-        pitch_func='',
-        bladeloading_func='',
-        airfoildata_file='',
-        airfoildata_path='',
-        airfoildata_file_type='',  # 1=flex5, 2=hawc2
-        bladegeo_file='',
-        bladegeo_path=''
-        ), # Below are re-definitions for ease of access
-        self.D=126.0
-        self.zRef=90.0
-
-    def nrel5mw_ct(self, u):
-        return np.interp(u, nrel5mw_ct_curve[:, 0], nrel5mw_ct_curve[:, 1])
-
-    def nrel5mw_power(self, u):
-        return np.interp(u, nrel5mw_power_curve[:, 0], nrel5mw_power_curve[:, 1])
-
-    def nrel5mw_rpm(self, u):
-        return np.interp(u, nrel5mw_rpm_curve[:, 0], nrel5mw_rpm_curve[:, 1])
 
 # Grid resolution settings
 # l: low, m: medium, f: fine (production), uf: ultra-fine (for grid study)
@@ -153,26 +178,7 @@ uf_res_grid = {
     'ad_out_reduce_a': 8, # Reduction of azimuthal grid points for AD output (integer)
     }
 
-# Wind farm settings
-from py_wake.examples.data.hornsrev1 import Hornsrev1Site
-from py_wake.site._site import UniformWeibullSite
-   
 
-class Single_Hornsrev1_wt(Hornsrev1Site):
-    def __init__(self):
-        Hornsrev1Site.__init__(self)
-        self.Ti=0.06
-        self.wt_x=[0]
-        self.wt_y=[0]
-        self.type_i=np.array([0])
-
-class TwobyTwo_Hornsrev1_wf(Hornsrev1Site):
-    def __init__(self, wt):
-        Hornsrev1Site.__init__(self)
-        self.Ti=0.06
-        self.wt_x=[0, 5*wt.D, 0, 5*wt.D]
-        self.wt_y=[0, 0, 5*wt.D, 5*wt.D]
-        self.type_i=np.array([0, 0, 0, 0])
 
 # Flow cases, [wind directions], [wind speeds]
 flow_case_1 = [270.0], [8.0] # Normal scenario
