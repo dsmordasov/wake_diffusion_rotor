5003dd24a24b4849a5ea753329bcd42d92d9d59f
Dmitrij <d.s.mordasov@gmail.com>
2022-03-23 15:16:28 +0100

    show power delta compared to baseline

--- pwes_comparative_wdr.py
+++ pwes_comparative_wdr.py
@@ -10,9 +10,12 @@ import os
 from py_wake_ellipsys.wind_farm_models.ellipsys import EllipSys 
 
 import config
+import pps
 
 run_grid = True
-design_name = 'flattened'
+design_name = 'new_design'
+
+baseline_power = 3.959274 # MW, flattened PWES at TI=0.06
 
 # DTU10MW
 from py_wake_ellipsys.wind_farm_models.ellipsys_lib.ellipsys_wind_turbines import EllipSysOneTypeWT
@@ -71,15 +74,14 @@ def run_pwes_simulation(
         ad_jou_rootdelta=0.35,
         ad_jou_roota=2.335,
         ad_jou_rootb=4.0,
-        grid=config.f_res_grid, # Grid resolution [l, m, f], see config.py
-        e3d_reslim=1e-5, # Convergence criteria
+        grid=config.l_res_grid, # Grid resolution [l, m, f], see config.py
+        e3d_reslim=1e-2, # Convergence criteria
         wf=config.Single_Hornsrev1_wt(), # Wind farm 
         ):
     
     start_time = time.time()
     
     wt = DTU10MW()
-    wf.Ti = 1e-2 # Set the turbulence intensity as close to 0 as in the DES simulation
     wd = [270] # [deg]
     ws = [8] # [m/s]
     
@@ -124,7 +126,12 @@ def run_pwes_simulation(
     WS_eff_ilk, power_ilk, ct_ilk = e3d_model.post_windfarm(wf.type_i, wd, ws) # what does ilk stand for?
     print("post_windfarm() started")
     
-    print(f"\n Power: {float(power_ilk)/1e6} MW \n")
+    new_power = float(power_ilk)/1e6 # MW
+    percentage_difference = (new_power - baseline_power) / baseline_power * 100
+    print(f"{'Baseline P: ':<20}" + f"{str(np.round(baseline_power, 4))} MW.")
+    print(f"{'{design_name} P: ':<20}" + f"{str(np.round(new_power, 4))} MW.")
+    print(f"{'Delta P: ':<19}" + f"{str(np.round(percentage_difference, 2))} %.")
+    
     end_time = time.time()
     runtime = round(end_time - start_time, 2)
     print(f"Simulation ran in {runtime}s.")
@@ -138,6 +145,10 @@ def run_pwes_simulation(
     # Calculate the averaged loads of the first AD, output results to <>adbladeloads.dat
     e3d_model.post_ad_bladeloads(wd, ws, np.array(wf.type_i))
     print("post_ad_bladeloads() started")
+    
+    # Post-process the ADVD.
+    
+    pps.pp_advd(design_name=design_name)
     
     return np.array([float(power_ilk), runtime])
 
