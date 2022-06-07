e68febac81ea6744f122547aec715bd79b314d5d
Dmitrij <d.s.mordasov@gmail.com>
2022-06-07 09:21:26 +0200

    Update pwes_comparative_wdr.py

--- pwes_comparative_wdr.py
+++ pwes_comparative_wdr.py
@@ -12,8 +12,8 @@ from py_wake_ellipsys.wind_farm_models.ellipsys import EllipSys
 import config
 import pps
 
-run_grid = True
-design_name = 'new_design'
+run_grid = False
+design_name = 'wdr_3'
 
 baseline_power = 3.959274 # MW, flattened PWES at TI=0.06
 
@@ -74,8 +74,8 @@ def run_pwes_simulation(
         ad_jou_rootdelta=0.35,
         ad_jou_roota=2.335,
         ad_jou_rootb=4.0,
-        grid=config.l_res_grid, # Grid resolution [l, m, f], see config.py
-        e3d_reslim=1e-2, # Convergence criteria
+        grid=config.f_res_grid, # Grid resolution [l, m, f], see config.py
+        e3d_reslim=1e-5, # Convergence criteria
         wf=config.Single_Hornsrev1_wt(), # Wind farm 
         ):
     
@@ -86,6 +86,7 @@ def run_pwes_simulation(
     ws = [8] # [m/s]
     
     e3d_model = EllipSys(wf, wt, wf.Ti, wt.zRef, # Required positional arguments
+        subcasename=f"_{design_name}",
         ad_force='3110', # Forcing method
         grid_cells1_D=grid['grid_cells1_D'], # Cells per rotor diameter in the inner domain [D], from 2 to 8
         grid_zFirstCell_D=0.5/wt.D, # Height of the first cell [D]
